79c5d3dc3173df8fee0debfec07da6842df27116
Shaheryar-ahmed-154 <shaheryar.ahmed.ten@gmail.com>
2025-02-02 13:31:48 +0500

    class code

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import { useState, useEffect,createContext,useRef, useMemo } from 'react';
+import { useState, useEffect,createContext,useRef, useMemo,useCallback } from 'react';
 import axios from 'axios'
 import ChildComponent from './ChildComponent'
 
@@ -9,20 +9,25 @@ console.log(":- rendering App Component")
   let [num1,setNum1] = useState(0);
   let [num2,setNum2] = useState(0);
 
-  function increaseNumber() {
+  const increaseNumber = useMemo(()=>{
     let result = num1
     // for (let index = 0; index < 999999999; index++) {
     //   result = result + 1;
     // }
     return result;
-  }
+  },[num1])
 
-  let newNumber = useMemo(()=>(increaseNumber()),[num1])
+///1111 , 1112
+  const getMultipliedNumber = useCallback(()=>{
+    return num1*3
+  },[num1])
+
+  // let newNumber = useMemo(()=>(increaseNumber()),[num1])
+
+  let newNumber = increaseNumber
 
 
   function handleChange(event){
-    console.log("name",event.target.name)
-    console.log(":- handleChange event",typeof event.target.value)
     // setResult(a+b)
     if(event.target.name === 'num1'){
       setNum1(+event.target.value)
@@ -35,7 +40,7 @@ console.log(":- rendering App Component")
 
   return (  
       <div style={{backgroundColor:'lightgray'}}>
-      <ChildComponent num1={num1} />
+        <h1>Appcomponent</h1>
 
       <div className='col-sm-12 col-md-6'>
       Num 1 : <input type="number" onChange={handleChange} name='num1'/>
@@ -45,6 +50,7 @@ console.log(":- rendering App Component")
       Num 2 : <input type="number" onChange={handleChange} name='num2'/>
       </div>
 
+      <ChildComponent num1={num1} getMultipliedNumber={getMultipliedNumber}/>
       <div>
       Result: {num1 + num2}
         </div>
--- src/ChildComponent.jsx
+++ src/ChildComponent.jsx
@@ -1,12 +1,18 @@
 import GrandChildComponent from "./GrandChildComponent";
 import { useContext,memo } from 'react'
-function ChildComponent({num1}) {
+function ChildComponent({num1,getMultipliedNumber}) {
+
+    const multipliedvalue = getMultipliedNumber()
+
 console.log(":- rendering Child Component")
     return (
         <div style={{ backgroundColor: 'lightblue' }}>
             <h1>Child Component</h1>
             <div>
                 num1 : {num1}
+            </div>
+            <div>
+            multipliedvalue : {multipliedvalue}
             </div>
         </div>
     )
