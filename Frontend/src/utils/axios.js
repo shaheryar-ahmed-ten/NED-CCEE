0fffa4b1b729d32b1fb3d27e0b6462ee7c9edd66
Shaheryar-ahmed-154 <shaheryar.ahmed.ten@gmail.com>
2025-03-01 13:50:10 +0500

    api interface

---
+++ Frontend/src/utils/axios.js
@@ -0,0 +1,21 @@
+import axios from "axios";
+
+const api = axios.create({
+    baseURL: "http://localhost:3000/api",
+});
+
+// Request interceptor to add the token to the headers
+api.interceptors.request.use(
+    (config) => {
+        const token = JSON.parse(localStorage.getItem("authToken"));
+        if (token) {
+            config.headers["Authorization"] = `Bearer ${token}`;
+        }
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+);
+
+export default api;
